c3e0bccf31a04dfe5390685b753f63552d1cdbb9
Seyoon Ko <kos@ucla.edu>
2023-12-21 18:09:11 -0800

    fix distributed mode

--- src/kernels.jl
+++ src/kernels.jl
@@ -118,82 +118,83 @@ for (RealType, RealCType, IntCType, RealCVectorType8, RealCVectorType2, ZERO, de
         RealCVectorType2 = $RealCVectorType2
         ZERO = $ZERO
         return $defs * """
-        #pragma OPENCL EXTENSION cl_khr_fp64 : enable
-        __kernel void computeLikContribs(__global const $RealCVectorType2 *locations,
-                                        __global const $RealCType *times,
-                                        __global $RealCType *likContribs,
-                                        const $RealCType sigmaXprec,
-                                        const $RealCType tauXprec,
-                                        const $RealCType tauTprec,
-                                        const $RealCType omega,
-                                        const $RealCType theta,
-                                        const $RealCType mu0,
-                                        const int dimX,
-                                        const uint locationStart,
-                                        const uint locationEnd,
-                                        const uint locationCount) {
-            const uint i = get_group_id(0);
-            const uint lid = get_local_id(0);
-            uint j = locationStart + get_local_id(0);
-            __local $RealCType scratch[$TPB];
-            __local $RealCType scratch2[$TPB];
-            const $RealCVectorType2 vectorI = locations[i];
-            const $RealCType timeI = times[i];
-            $RealCType        sum = $ZERO;
-            $RealCType mu0TauXprecDTauTprec = mu0 * pow(tauXprec,dimX) * tauTprec;
-            $RealCType thetaSigmaXprecDOmega = theta * pow(sigmaXprec,dimX) * omega;
-            while (j < locationEnd) {
-                const $RealCType timDiff = timeI - times[j];
-                const $RealCVectorType2 vectorJ = locations[j];
-                const $RealCVectorType2 difference = vectorI - vectorJ;
+            #pragma OPENCL EXTENSION cl_khr_fp64 : enable
+            __kernel void computeLikContribs(__global const $RealCVectorType2 *locations,
+                                            __global const $RealCType *times,
+                                            __global $RealCType *likContribs,
+                                            const $RealCType sigmaXprec,
+                                            const $RealCType tauXprec,
+                                            const $RealCType tauTprec,
+                                            const $RealCType omega,
+                                            const $RealCType theta,
+                                            const $RealCType mu0,
+                                            const int dimX,
+                                            const uint locationStart,
+                                            const uint locationEnd,
+                                            const uint locationCount) {
+                const uint i = locationStart + get_group_id(0);
+                const uint gid = get_group_id(0);
+                const uint lid = get_local_id(0);
+                uint j = get_local_id(0);
+                __local $RealCType scratch[$TPB];
+                __local $RealCType scratch2[$TPB];
+                const $RealCVectorType2 vectorI = locations[i];
+                const $RealCType timeI = times[i];
+                $RealCType        sum = $ZERO;
+                $RealCType mu0TauXprecDTauTprec = mu0 * pow(tauXprec,dimX) * tauTprec;
+                $RealCType thetaSigmaXprecDOmega = theta * pow(sigmaXprec,dimX) * omega;
+                while (j < locationCount) {
+                    const $RealCType timDiff = timeI - times[j];
+                    const $RealCVectorType2 vectorJ = locations[j];
+                    const $RealCVectorType2 difference = vectorI - vectorJ;
 
-                const $RealCType distancesq = dot(difference.lo, difference.lo) + dot(difference.hi, difference.hi);
+                    const $RealCType distancesq = dot(difference.lo, difference.lo) + dot(difference.hi, difference.hi);
 
-                const $RealCType innerContrib = mu0TauXprecDTauTprec *
-                                        ((timDiff != 0) ? pdfpdf_1sq(distancesq * tauXprec * tauXprec, timDiff * tauTprec) : $ZERO) +
-                                        //pdf(distance * tauXprec) * pdf(timDiff*tauTprec) : $ZERO) +
-                                        thetaSigmaXprecDOmega *
-                                        ((timDiff > 0) ? exppdf_2sq(omega * timDiff, distancesq * sigmaXprec * sigmaXprec) : $ZERO);
-                                        //exp(-omega * timDiff) * pdf(distance * sigmaXprec) : $ZERO);
-                sum += innerContrib;
-                j += $TPB;
-            }
+                    const $RealCType innerContrib = mu0TauXprecDTauTprec *
+                                            ((timDiff != 0) ? pdfpdf_1sq(distancesq * tauXprec * tauXprec, timDiff * tauTprec) : $ZERO) +
+                                            //pdf(distance * tauXprec) * pdf(timDiff*tauTprec) : $ZERO) +
+                                            thetaSigmaXprecDOmega *
+                                            ((timDiff > 0) ? exppdf_2sq(omega * timDiff, distancesq * sigmaXprec * sigmaXprec) : $ZERO);
+                                            //exp(-omega * timDiff) * pdf(distance * sigmaXprec) : $ZERO);
+                    sum += innerContrib;
+                    j += $TPB;
+                }
 
-            scratch[lid] = sum;
-            scratch2[lid] = sum;
+                scratch[lid] = sum;
+                scratch2[lid] = sum;
 
-            barrier(CLK_LOCAL_MEM_FENCE);
-            for(int k = 1; k < $TPB; k <<= 1) {
                 barrier(CLK_LOCAL_MEM_FENCE);
-                uint mask = (k << 1) - 1;
-                if ((lid & mask) == 0) {
-                    if (scratch2[lid] < scratch2[lid + k])
-                        scratch2[lid] = scratch2[lid + k];
+                for(int k = 1; k < $TPB; k <<= 1) {
+                    barrier(CLK_LOCAL_MEM_FENCE);
+                    uint mask = (k << 1) - 1;
+                    if ((lid & mask) == 0) {
+                        if (scratch2[lid] < scratch2[lid + k])
+                            scratch2[lid] = scratch2[lid + k];
+                    }
+                }
+
+                barrier(CLK_LOCAL_MEM_FENCE);
+                $RealCType maximum = max(scratch2[0], ($RealCType) 1e-40);
+                scratch[lid] = scratch[lid] / maximum;
+                barrier(CLK_LOCAL_MEM_FENCE);
+                for(int k = 1; k < $TPB; k <<= 1) {
+                    barrier(CLK_LOCAL_MEM_FENCE);
+                    uint mask = (k << 1) - 1;
+                    if ((lid & mask) == 0) {
+                        scratch[lid] += scratch[lid + k];
+                    }
+                }
+                barrier(CLK_LOCAL_MEM_FENCE);
+                scratch[0] = max(scratch[0], ($RealCType) 1e-40);
+
+                barrier(CLK_LOCAL_MEM_FENCE);
+                if (lid == 0) {
+                    likContribs[gid] = log(maximum) + log(scratch[0]) + theta *
+                    ( exp(-omega*(times[locationCount-1]-times[i]))-1 ) - 
+                    mu0 * ( cdf((times[locationCount-1]-times[i])*tauTprec)-
+                    cdf(-times[i]*tauTprec) ) ;
                 }
             }
-
-            barrier(CLK_LOCAL_MEM_FENCE);
-            $RealCType maximum = max(scratch2[0], ($RealCType) 1e-40);
-            scratch[lid] = scratch[lid] / maximum;
-            barrier(CLK_LOCAL_MEM_FENCE);
-            for(int k = 1; k < $TPB; k <<= 1) {
-                barrier(CLK_LOCAL_MEM_FENCE);
-                uint mask = (k << 1) - 1;
-                if ((lid & mask) == 0) {
-                    scratch[lid] += scratch[lid + k];
-                }
-            }
-            barrier(CLK_LOCAL_MEM_FENCE);
-            scratch[0] = max(scratch[0], ($RealCType) 1e-40);
-
-            barrier(CLK_LOCAL_MEM_FENCE);
-            if (lid == 0) {
-                likContribs[i] = log(maximum) + log(scratch[0]) + theta *
-                ( exp(-omega*(times[locationCount-1]-times[i]))-1 ) - 
-                mu0 * ( cdf((times[locationCount-1]-times[i])*tauTprec)-
-                cdf(-times[i]*tauTprec) ) ;
-            }
-        }
             """
         end
     end
